72201979c812f1695ae2f3312b4c9d56a7f5da94
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2024-02-18 00:06:32 +0300

    migration1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    about = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=150, verbose_name='Название')
+    product_about = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='products', verbose_name='Изображение', null=True, blank=True)
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата последнего изменения', auto_now=True)
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.product_name} {self.product_price} {self.product_category}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
